93f71848c493f6898e2b257d26451f8581d90139
Miklas Karjalainen <giffidev@gmail.com>
2021-07-14 23:05:51 +0300

    barebones mbc1 support.

--- src/emu/gbCart.cpp
+++ src/emu/gbCart.cpp
@@ -4,73 +4,163 @@
 
 namespace Giffi
 {   
-    std::string gbCart::GetGameName()
+
+std::string gbCart::GetGameName()
+{
+    char name[12] = "";
+    memcpy( &name, mCart + 0x134, sizeof(char) * 11 );
+    return std::string(name);
+}
+
+// 1 rom bank is 16kb
+uint16_t gbCart::GetRomBankCount()
+{
+    return mCart[0x148] * 4;
+}
+
+// In Kb (KiloBytes), 1 ram bank is 8kb
+uint16_t gbCart::GetRamSize() 
+{
+    switch (mCart[0x149])
     {
-        char name[11] = "";
-        memcpy( &name, mCart + 0x134, sizeof(char) * 11 );
-        return std::string(name);
+        case 0x1: return 2;
+        case 0x2: return 8;
+        case 0x3: return 32;
+        case 0x4: return 128;
+        case 0x5: return 256;
+        default: return 0;
     }
+}
 
-    void gbCart::Reset()
+gbCartType gbCart::GetCartType()
+{
+    switch (mCart[0x147])
     {
+        case 0x01: // MBC1
+        case 0x02: // MBC1+RAM
+        case 0x03: // MBC1+RAM+BATTERY
+            return gbCartType::MBC1;
 
-    }
-
-    void gbCart::LoadRom(std::string _path)
-    {
-        // Init Cart
-        memset(mCart,0,sizeof(mCart));
-        FILE *in;
-        in = fopen( _path.c_str(), "rb" );
-        fread(mCart, 1, 0x200000, in);
-        fclose(in); 
-        mCurRomBank = 1;
+        case 0x04: // MBC2
+        case 0x05: // MBC2+BATTERY
+            return gbCartType::MBC2;
         
-        // Init Ram
-        // memset( &mRam,0,sizeof(mRam) );
-        mCurRamBank = 0;
+        //case 0x0F: // MBC3+TIMER+BATTERY
+        //case 0x10: // MBC3+TIMER+BATTERY+RAM
+        case 0x11: // MBC3
+        case 0x12: // MBC3+RAM
+        case 0x13: // MBC3+RAM+BATTERY
+            return gbCartType::MBC3;
 
-        switch (mCart[0x147])
-        {
-            case 1: // MBC1
-            case 2: // MBC1+RAM
-            case 3: // MBC1+RAM+BATTERY
-                mMBC = CartType::MBC1;
-                break;
-            
-            case 4: // MBC2
-            case 5: // MBC2+BATTERY
-                mMBC = CartType::MBC2;
-                break;
-            
-            default: // None or unsupported
-                mMBC = CartType::None;
-                break;
-        } 
-    }
+        default: // None or unsupported
+            return gbCartType::None;
+    } 
+}
 
-    uint8_t& gbCart::ReadByte(uint16_t _addr)
+void gbCart::Reset()
+{
+    mCurRomBank = 1;
+    mCurRamBank = 0;
+    memset(&mRam, 0xFF, sizeof(mRam));
+    mRamEnable = false;
+    mMBC = GetCartType();
+}
+
+void gbCart::LoadRom(std::string _path)
+{
+    // Init Cart
+    memset(mCart,0,sizeof(mCart));
+    FILE *in;
+    in = fopen( _path.c_str(), "rb" );
+    fread(mCart, 1, 0x200000, in);
+    fclose(in); 
+    
+    // Reset cart variables to defaults
+    Reset();
+}
+
+uint8_t gbCart::ReadByte(uint16_t _addr)
+{
+    if ( _addr < 0x4000) // Cartridge
     {
-        // are we reading from the rom memory bank?
-        if ((_addr>=0x4000) && (_addr < 0x8000))
-        {
-            uint16_t new_addr = (_addr - 0x4000) + (0x4000 * mCurRomBank);
-            return mCart[new_addr];
-        }
         return mCart[_addr];
     }
-
-    void gbCart::WriteByte(uint16_t _addr, uint8_t _data)
+    else if ((_addr>=0x4000) && (_addr < 0x8000))   // Rom bank
     {
-        if ( _addr >= 0x2000 && _addr < 0x4000) // ROM Change
-        {
-            if (_data == 0x00) {mCurRomBank = 0x01; return;}
-            if (_data == 0x20) {mCurRomBank = 0x21; return;}
-            if (_data == 0x40) {mCurRomBank = 0x41; return;}
-            if (_data == 0x60) {mCurRomBank = 0x61; return;}
-            mCurRomBank = (_data & 0x1F);
-        }
+        uint16_t new_addr = (_addr - 0x4000) + (0x4000 * mCurRomBank);
+        return mCart[new_addr];
     }
-
-
+    else if ((_addr >= 0xA000) && (_addr < 0xC000)) // Ram banks
+    {
+        uint16_t new_addr = _addr - 0xA000;
+        return mRam[new_addr + (mCurRamBank*0x2000)]; 
+    }
+    else
+    {
+        return 0xFF;
+    }
 }
+
+void gbCart::WriteByte(uint16_t _addr, uint8_t _data)
+{
+    switch (mMBC)
+    {
+        case gbCartType::MBC1:
+        {
+            if ( _addr < 0x2000)                         // Ram Enable
+            {
+                mRamEnable = _data > 0;
+            }
+            else if (_addr < 0x4000) // ROM Change
+            {
+                mCurRomBank &= 224;
+                mCurRomBank |= _data & 0x1F;
+                if (mCurRomBank == 0x00 || mCurRomBank == 0x20 || // Cant be any of these.
+                    mCurRomBank == 0x40 || mCurRomBank == 0x60) { mCurRomBank++; }
+            }
+            else if (_addr < 0x6000) // Set upper bits of rom bank number
+            {
+                if (mRomMode)
+                {
+                    mCurRomBank &= ~(0b11 << 5);
+                    mCurRomBank |= (_data & 3) << 5;
+                }
+                else
+                {
+                    mCurRamBank = _data & 3;
+                }
+            }
+            else if (_addr < 0x8000) // Ram / Rom Change
+            {
+                mRomMode = _data > 0;
+            }
+            else if ( ( _addr >= 0xA000 ) && ( _addr < 0xC000 ) ) // Ram Bank
+            {
+                if (!mRamEnable) { return; }
+                uint16_t new_addr = _addr - 0xA000;
+                mRam[new_addr + (mCurRamBank * 0x2000)] = _data;
+            }
+            break;
+        }
+        case gbCartType::MBC2:
+        {
+            if (_addr < 0x2000)
+            {
+                mRamEnable = _data > 0;
+            }
+            else if (_addr < 0x4000)
+            {
+                mCurRomBank = _data & 0x1F;
+                if (mCurRomBank == 0x00 || mCurRomBank == 0x20 || // Cant be any of these.
+                    mCurRomBank == 0x40 || mCurRomBank == 0x60) { mCurRomBank++; }
+            }
+            break;
+        }
+        default: break;
+    }
+    
+}
+
+
+
+} // Namespace 
--- src/emu/gbCart.hpp
+++ src/emu/gbCart.hpp
@@ -5,36 +5,41 @@
 namespace Giffi
 {
 
-    enum CartType
-    {
-        None = 0,
-        MBC1 = 1,
-        MBC2 = 2,
-    };
+enum gbCartType // meaby turn this into a class instead? Probably would clean up these else ifs and switches a bit.
+{
+    None = 0,
+    MBC1 = 1,
+    MBC2 = 2,
+    MBC3 = 3,
+};
 
-    class gbGameBoy;
-    class gbCart
-    {
-    public:
-        gbCart()  = default;
-        ~gbCart() = default;
+class gbGameBoy;
+class gbCart
+{
+public:
+    gbCart()  = default;
+    ~gbCart() = default;
 
-        std::string GetGameName();
-    private:
-        void Reset();
-        void LoadRom(std::string _path);
-        uint8_t& ReadByte(uint16_t _addr);
-        void     WriteByte(uint16_t _addr, uint8_t _data);
-    
-    private:
-        CartType mMBC = CartType::None;
-        uint8_t  mCart[0x200000]; 
-        uint8_t  mRam[0x8000];
-        uint8_t  mCurRamBank = 0;
-        uint16_t mCurRomBank = 1;
+    std::string GetGameName();
+    uint16_t    GetRomBankCount();
+    uint16_t    GetRamSize();
+    gbCartType  GetCartType();
+private:
+    void Reset();
+    void LoadRom(std::string _path);
+    uint8_t ReadByte(uint16_t _addr);
+    void    WriteByte(uint16_t _addr, uint8_t _data);
+private:
+    gbCartType mMBC = gbCartType::None;
+    uint8_t  mCart[0x200000]; 
+    uint8_t  mRam[0x8000];
+    uint16_t  mCurRomBank = 1;
+    uint8_t  mCurRamBank = 0;
+    bool     mRamEnable = false;
+    bool     mRomMode   = true;
 
-        friend gbGameBoy;
-    };
+    friend gbGameBoy;
+};
 
 
 }
