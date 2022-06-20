405c53ee81a7518a33408a6b9d0f15cb8ccaa8b0
Miklas Karjalainen <giffidev@gmail.com>
2022-06-20 13:31:01 +0300

    fix windows build

--- src/application/gui/rebindButton.cpp
+++ src/application/gui/rebindButton.cpp
@@ -2,32 +2,32 @@
 #include "rebindButton.hpp"
 #include "../imgui/imgui.h"
 
-using namespace GeimBoi;
+namespace GeimBoi {
+    rebindButton* rebindButton::sCurrentlyBinding = nullptr;
+    uint16_t rebindButton::LastKeyDown = 0;
 
-rebindButton* rebindButton::sCurrentlyBinding = nullptr;
-uint16_t rebindButton::LastKeyDown = 0;
-
-rebindButton::rebindButton(const char* button_text, uint16_t& button_setting)
-    : mButtonText(button_text), mSetting(button_setting)
-{
-
-}
-
-void rebindButton::Draw()
-{
-    const bool binding_this = sCurrentlyBinding == this;
-    const char* btn_text = binding_this ? "..." : SDL_GetScancodeName((SDL_Scancode)mSetting);
-    std::string t = mButtonText + ": [" + btn_text + "]";
-
-    if (ImGui::Button( t.c_str(), {140, 18}))
+    rebindButton::rebindButton(const char* button_text, uint16_t& button_setting)
+        : mButtonText(button_text), mSetting(button_setting)
     {
-        sCurrentlyBinding = this;
-        LastKeyDown = 0;
+
     }
 
-    if (binding_this && LastKeyDown)
+    void rebindButton::Draw()
     {
-        mSetting = LastKeyDown;
-        sCurrentlyBinding = nullptr;
+        const bool binding_this = sCurrentlyBinding == this;
+        const char* btn_text = binding_this ? "..." : SDL_GetScancodeName((SDL_Scancode)mSetting);
+        std::string t = mButtonText + ": [" + btn_text + "]";
+
+        if (ImGui::Button(t.c_str(), { 140, 18 }))
+        {
+            sCurrentlyBinding = this;
+            LastKeyDown = 0;
+        }
+
+        if (binding_this && LastKeyDown)
+        {
+            mSetting = LastKeyDown;
+            sCurrentlyBinding = nullptr;
+        }
     }
 }
