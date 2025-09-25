0a196fa4c9a6f5b03245657a16d58bc917f5a375
Miklas Karjalainen <miklas.karjalainen@gmail.com>
2025-09-25 13:17:49 +0300

    add disassembler

--- src/application/entry_point.cpp
+++ src/application/entry_point.cpp
@@ -13,7 +13,30 @@ namespace GeimBoi {
         gb_emu_advance_opcode(&emu);
 
         std::printf("%X\n", gb_emu_read_u16(&emu, GB_REG_PC(emu.cpu.regs)));
-        std::printf("%X\n", GB_REG_PC(emu.cpu.regs));
+
+        for (int j = 0; j < 255;){
+            u8 opcode = gb_emu_read_u8(&emu, GB_REG_PC(emu.cpu.regs) + j);
+            std::printf("[0x%04X] %s ", GB_REG_PC(emu.cpu.regs) + j, gb_opcode_asm(opcode));
+
+            u8 size = gb_opcode_size(opcode);
+            switch(size)
+            {
+                case 3: {
+                    std::printf("[0x%04X]", gb_emu_read_u16(&emu, GB_REG_PC(emu.cpu.regs) + j + 1));
+                    break;
+                }
+                case 2: {
+                    std::printf("[0x%02X]", gb_emu_read_u8(&emu, GB_REG_PC(emu.cpu.regs) + j + 1));
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+            j += size;
+            std::printf("\n");
+        }
+
         std::printf("%s\n", gb_cart_get_name(&emu.cart));
         std::printf("%i\n", gb_verify_checksums(&emu.cart));
         return 0;
---
+++ src/core/gbDisassembler.h
@@ -0,0 +1,4 @@
+#include "gbReg.h"
+
+const char* gb_opcode_asm(u8 opcode);
+u8 gb_opcode_size(u8 opcode);
