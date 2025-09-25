219871a0d06a35cdd0ff840310f021850de96680
Miklas Karjalainen <miklas.karjalainen@gmail.com>
2025-09-25 12:24:10 +0300

    Create log.h

---
+++ src/core/log.h
@@ -0,0 +1,61 @@
+#ifndef _GB_PRINT_H
+#define _GB_PRINT_H
+
+#ifdef _GB_DISABLE_ANSI_CODES
+	#define GB_COLOR_RESET
+	#define GB_COLOR_RED
+	#define GB_COLOR_YELLOW
+	#define GB_COLOR_GREEN
+	#define GB_COLOR_CYAN
+	#define GB_COLOR_BOLDRED
+#else
+	#define GB_COLOR_RESET "\x1b[0m"
+	#define GB_COLOR_RED "\x1b[31m"
+	#define GB_COLOR_YELLOW "\x1b[33m"
+	#define GB_COLOR_GREEN "\x1b[32m"
+	#define GB_COLOR_CYAN "\x1b[36m"
+	#define GB_COLOR_BOLDRED "\x1b[1;31m"
+#endif
+
+#if defined(_GB_DISABLE_STDLIB) || defined(_GB_DISABLE_PRINT)
+	#define GB_INFO(...)
+	#define GB_WARN(...)
+	#define GB_ERROR(...)
+#else
+	#include <stdio.h>
+
+	#ifndef _GB_LOG_OUT
+		#define _GB_LOG_OUT stdout
+	#endif
+	#ifndef _GB_LOG_ERR
+		#define _GB_LOG_ERR stderr
+	#endif
+
+	#define GB_INFO(fmt, ...)                                                                      \
+		do {                                                                                       \
+			fprintf(_GB_LOG_OUT, GB_COLOR_CYAN "[INFO] " GB_COLOR_RESET fmt "\n", ##__VA_ARGS__);  \
+		} while (0)
+
+	#define GB_WARN(fmt, ...)                                                                      \
+		do {                                                                                       \
+			fprintf(                                                                               \
+				_GB_LOG_OUT, GB_COLOR_YELLOW "[WARN] " fmt GB_COLOR_RESET "\n", ##__VA_ARGS__      \
+			);                                                                                     \
+		} while (0)
+
+	#define GB_ERROR(fmt, ...)                                                                     \
+		do {                                                                                       \
+			fprintf(_GB_LOG_ERR, GB_COLOR_RED "[ERROR] " fmt GB_COLOR_RESET "\n", ##__VA_ARGS__);  \
+		} while (0)
+
+	#define GB_FATAL(fmt, ...)                                                                     \
+		do {                                                                                       \
+			fprintf(                                                                               \
+				_GB_LOG_ERR, GB_COLOR_BOLDRED "[FATAL] " GB_COLOR_RESET fmt "\n", ##__VA_ARGS__    \
+			);                                                                                     \
+			exit(EXIT_FAILURE);                                                                    \
+		} while (0)
+
+#endif
+
+#endif
